fb62482172096b1bb9b4bb88a0d04aca0fe0697b
Vince <vince@cvq.cc>
2016-05-16 14:43:45 -0700

    add login functionality, add login tests

---
+++ test/controllers/session_controller_test.exs
@@ -0,0 +1,43 @@
+defmodule Phxblog.SessionControllerTest do
+  use Phxblog.ConnCase
+  alias Phxblog.User
+
+  setup do
+    User.changeset(%User{}, %{username: "test",
+                              password: "test",
+                              password_confirmation: "test",
+                              email: "test@test.com"})
+    |>Repo.insert
+    conn = conn()
+    {:ok, conn: conn}
+  end
+
+  test "shows the login form", %{conn: conn} do
+    conn = get conn, session_path(conn, :new)
+    assert html_response(conn, 200) =~ "Login"
+  end
+
+  test "creates a new session for a valid user", %{conn: conn} do
+    conn = post conn, session_path(conn, :create),
+            user: %{ username: "test", password: "test"}
+    assert get_session(conn, :current_user)
+    assert get_flash(conn, :info) == "Sign in successful!"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
+  test "does not create a new session for an incorrect password", %{conn: conn} do
+    conn = post conn, session_path(conn, :create),
+            user: %{ username: "test", password: "wrong"}
+    refute get_session(conn, :current_user)
+    assert get_flash(conn, :info) == "Invalid username/password combo!"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
+  test "does not create a session if the user does not exist", %{conn: conn} do
+    conn = post conn, session_path(conn, :create),
+            user: %{ username: "foo", password: "test"}
+    assert get_flash(conn, :error) == "Invalid username/password combo!"
+    assert redirected_to(conn) == page_path(conn, :index)
+  end
+
+end
--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,9 +1,53 @@
 defmodule Phxblog.SessionController do
   use Phxblog.Web, :controller
+  import Comeonin.Bcrypt, only: [checkpw: 2]
 
   alias Phxblog.User
 
+  plug :scrub_params, "user" when action in [:create]
+
   def new(conn, _params) do
+    #pass to the render function
+    #pass it the connection (conn)
+    #the template to be rendered (without the eex)
+    #and the changeset
     render conn, "new.html", changeset: User.changeset(%User{})
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    if is_nil(user_params["username"]) do
+      conn
+      |> put_flash(:error, "Invalid username/password combo!")
+      |> redirect(to: page_path(conn, :index))
+    else
+      #this part pulls the first applicable user from Ecto.Repo that
+      #has a matching username
+      Repo.get_by(User, username: user_params["username"])
+      |> sign_in(user_params["password"], conn)
+    end
+  end
+
+  #the functions get run in order. so this one will run when the user is nil
+  #note that there is an error in the create function when the
+  #user is nil. so it doesn't seem like this function ever gets called.
+  #put an if into the create function to check for nil?
+  defp sign_in(user, password, conn) when is_nil(user) do
+    conn
+    |> put_flash(:error, "Invalid username/password combo!")
+    |> redirect(to: page_path(conn, :index))
+  end
+
+  defp sign_in(user, password, conn) do
+    if checkpw(password, user.password_digest) do
+      conn
+      |> put_session(:current_user, %{id: user.id, username: user.username})
+      |> put_flash(:info, "Sign in successful!")
+      |> redirect(to: page_path(conn, :index))
+    else
+      conn
+      |> put_session(:current_user, nil)
+      |> put_flash(:info, "Invalid username/password combo!")
+      |> redirect(to: page_path(conn, :index))
+    end
   end
 end
